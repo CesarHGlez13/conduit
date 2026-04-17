53c36622f55ebf4c4382797d9c6df8dfd99bf2ba
CesGonAI Builder <cesgonai-builder@example.com>
2026-04-16 23:50:04 -0600

    fix: update tunnel URL to graphics-tuning-thinks-labs

--- lib/features/auth/views/server_connection_page.dart
+++ lib/features/auth/views/server_connection_page.dart
@@ -37,7 +37,7 @@ class ServerConnectionPage extends ConsumerStatefulWidget {
 
 class _ServerConnectionPageState extends ConsumerState<ServerConnectionPage> {
   final _formKey = GlobalKey<FormState>();
-  final TextEditingController _urlController = TextEditingController(text: 'https://participants-original-deeply-responses.trycloudflare.com');
+  final TextEditingController _urlController = TextEditingController(text: 'https://graphics-tuning-thinks-labs.trycloudflare.com');
   final Map<String, String> _customHeaders = {};
   final TextEditingController _headerKeyController = TextEditingController();
   final TextEditingController _headerValueController = TextEditingController();
